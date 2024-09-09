182c1edc4462684909109289d81bbf5571a1b39b
Chennuhunaysubhash <hunaysubhash97@gmail.com>
2024-09-09 21:10:41 +0530

    Initial implementaton of string calculator TDD kata

---
+++ ttd_string_calculator/src/main/java/org/example/StringCalculator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class StringCalculator {
+    public static int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return -1; // Placeholder
+    }
+}
---
+++ ttd_string_calculator/src/main/java/org/example/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCalculatorTest {
+    @Test
+    public void testEmptyString() {
+        assertEquals(0, StringCalculator.add(""));
+    }
+}
